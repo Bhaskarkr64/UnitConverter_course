33213acc6e13505f057049438e5deb8ebf21da7e
Bhaskarkr64 <bhaskarkr65@gmail.com>
2025-07-03 12:39:09 +0530

    new changes

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,28 +23,42 @@ class MainActivity : ComponentActivity() {
         setContent {
             UnitConverterTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    UnitConverter()
                 }
             }
         }
     }
 }
-
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun UnitConverter(){
+    Column{
+        Text("Unit Converter")
+        OutlinedTextField(value = "", onValueChange = {})
+    }
+    Row{
 
+    }
+    Text("Result")
+}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    UnitConverterTheme {
+//        Greeting("Android")
+//    }
+//}
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun UnitConverterPreview(){
     UnitConverterTheme {
-        Greeting("Android")
+        UnitConverter()
     }
 }
