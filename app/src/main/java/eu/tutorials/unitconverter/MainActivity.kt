44b1ab3b6d839c2e208063c2c57a573275c1ccdb
Bhaskarkr64 <bhaskarkr65@gmail.com>
2025-07-03 22:42:30 +0530

    box and drop down menu added previous button removed

--- app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
+++ app/src/main/java/eu/tutorials/unitconverter/MainActivity.kt
@@ -5,11 +5,15 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -38,11 +42,19 @@ fun UnitConverter(){
         Text("Unit Converter")
         OutlinedTextField(value = "", onValueChange = {})
         Row{
-            val context = LocalContext.current
-            Button(onClick = {
-                Toast.makeText(context,"Thanks for clicking!",Toast.LENGTH_LONG).show() }) {
-                Text("Click me!")
+            Box{
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
             }
+            Box{
+                Button(onClick = {}) {
+                    Text("Select")
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = "arrow down")
+                }
+            }
+
 
         }
         Text("Result:")
